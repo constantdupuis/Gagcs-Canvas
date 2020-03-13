8b60ba215e1b567da0ba28c266588b5508e3165d
Constant Dupuis <constant.dupuis@alterface.com>
2020-03-13 14:18:40 +0100

    Back to bezier with  slowlly moving particles but still not that good
    I will get back to curves ...

---
+++ SandSpline/sketch/libs/models.ts
@@ -0,0 +1,9 @@
+class SplineControllers {
+  ctrl1: p5.Vector;
+  ctrl2: p5.Vector;
+
+  constructor(ctrl1: p5.Vector, ctrl2: p5.Vector) {
+    this.ctrl1 = ctrl1;
+    this.ctrl2 = ctrl2;
+  }
+}
--- SandSpline/sketch/sketch.ts
+++ SandSpline/sketch/sketch.ts
@@ -8,6 +8,8 @@ let yRandomVariance = 0;
 
 let drawCount = 100;
 let c: p5.Color;
+let anchors: p5.Vector[] = [];
+let splineControllers: SplineControllers[] = [];
 
 /**
  * setup
@@ -23,40 +25,25 @@ function setup() {
   lineHeight = windowHeight * 0.45;
   section_lenght = (windowWidth - (2 * margin)) / nb_sections;
 
-  xRandomVariance = 150;
-  yRandomVariance = 150;
+  xRandomVariance = 50;
+  yRandomVariance = 30;
   drawCount = 500;
 
-  let c = color("red");
-  c.setAlpha(20);
-  stroke(c);
-  noFill();
-  strokeWeight(1);
+  for (let i = 0; i < nb_sections + 1; i++) {
+    let x1 = margin + (i * section_lenght);
+    let x2 = margin + ((i + 1) * section_lenght);
 
-  // let c = color("white");
-  // c.setAlpha(128);
-  // stroke(c);
-  // noFill();
-  // strokeWeight(2);
+    let randomWeight = i / nb_sections;
 
-  // beginShape();
-  // for (let i = 0; i < nb_sections; i++) {
-
-  //   let x1 = margin + (i * section_lenght);
-  //   let x2 = margin + ((i + 1) * section_lenght);
-
-  //   let randomWeight = i / nb_sections;
-  //   let ctrlP1x = x1 + (randomGaussian(0, xRandomVariance)) * randomWeight;
-  //   let ctrlP1y = lineHeight + (randomGaussian(0, xRandomVariance * randomWeight)) * randomWeight;
-  //   let ctrlP2x = x2 + (randomGaussian(0, xRandomVariance)) * randomWeight;
-  //   let ctrlP2y = lineHeight + (randomGaussian(0, xRandomVariance)) * randomWeight;
-
-  //   curveVertex(x1, ctrlP1y);
-
-  // }
-  // endShape();
+    let ctrlP1x = x1 + 10 + (random(0, xRandomVariance)) * randomWeight;
+    let ctrlP1y = lineHeight + (randomGaussian(0, xRandomVariance * randomWeight)) * randomWeight;
+    let ctrlP2x = x2 - 10 - (random(0, xRandomVariance)) * randomWeight;
+    let ctrlP2y = lineHeight + (randomGaussian(0, xRandomVariance)) * randomWeight;
 
 
+    anchors.push(createVector(x1, lineHeight));
+    if (i < nb_sections) splineControllers.push(new SplineControllers(createVector(ctrlP1x, ctrlP1y), createVector(ctrlP2x, ctrlP2y)));
+  }
 
   console.log("<setup");
 }
@@ -66,24 +53,60 @@ function setup() {
  */
 function draw() {
 
+  //background("white");
 
+  // stroke("red");
+  // fill("red");
 
-  beginShape();
-  for (let i = 0; i < nb_sections; i++) {
+  // for (let i = 0; i < anchors.length - 1; i++) {
+  //   let anchorA = anchors[i];
+  //   let anchorB = anchors[i + 1];
 
-    let x1 = margin + (i * section_lenght);
-    let x2 = margin + ((i + 1) * section_lenght);
+  //   strokeWeight(4);
+  //   circle(anchorA.x, anchorA.y, 5);
+  //   if (i == anchors.length - 2) circle(anchorB.x, anchorB.y, 5);
 
-    let randomWeight = i / nb_sections;
-    let ctrlP1x = x1 + (randomGaussian(0, xRandomVariance)) * randomWeight;
-    let ctrlP1y = lineHeight + (randomGaussian(0, xRandomVariance * randomWeight)) * randomWeight;
-    let ctrlP2x = x2 + (randomGaussian(0, xRandomVariance)) * randomWeight;
-    let ctrlP2y = lineHeight + (randomGaussian(0, xRandomVariance)) * randomWeight;
+  //   strokeWeight(2);
+  //   line(anchorA.x, anchorA.y, anchorB.x, anchorB.y);
+  // }
 
-    curveVertex(ctrlP1x, ctrlP1y);
+  // let c = color("green");
+  // c.setAlpha(50);
+  // stroke(c);
+  // fill(c);
 
+  // for (let i = 0; i < anchors.length - 1; i++) {
+  //   let ctrls = splineControllers[i];
+  //   let randomWeight = i / (anchors.length - 1);
+
+  //   strokeWeight(4);
+  //   circle(ctrls.ctrl1.x + randomGaussian(0, 1.5) * randomWeight, ctrls.ctrl1.y + randomGaussian(0, 1.5) * randomWeight, 5);
+  //   circle(ctrls.ctrl2.x + randomGaussian(0, 1.5) * randomWeight, ctrls.ctrl2.y + randomGaussian(0, 1.5) * randomWeight, 5);
+  // }
+
+  let c = color("green");
+  c.setAlpha(1);
+  stroke(c);
+  noFill();
+
+  for (let i = 0; i < anchors.length - 1; i++) {
+    let ctrls = splineControllers[i];
+    let anchorA = anchors[i];
+    let anchorB = anchors[i + 1];
+    let randomWeight = i / (anchors.length - 1);
+
+    bezier(anchorA.x, anchorA.y,
+      ctrls.ctrl1.x, ctrls.ctrl1.y,
+      ctrls.ctrl2.x, ctrls.ctrl2.y,
+      anchorB.x, anchorB.y);
+
+    ctrls.ctrl1.x += randomGaussian(0, 5.5) * randomWeight;
+    ctrls.ctrl1.y += randomGaussian(0, 5.5) * randomWeight;
+    ctrls.ctrl2.x += randomGaussian(0, 5.5) * randomWeight;
+    ctrls.ctrl2.y += randomGaussian(0, 5.5) * randomWeight;
+
+    splineControllers[i] = ctrls;
   }
-  endShape();
 
   drawCount--;
   if (drawCount == 0) noLoop();

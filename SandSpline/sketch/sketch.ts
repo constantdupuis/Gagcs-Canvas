90d83aed9d6e2a4ab3eda4e2735cdde970771320
Constant Dupuis <constant.dupuis@alterface.com>
2020-03-13 12:52:10 +0100

    Try with curve, but still not there
    I got that, control points should be placed randomly first then slightly moved (randomly) from base position on each frame

--- SandSpline/sketch/sketch.ts
+++ SandSpline/sketch/sketch.ts
@@ -7,6 +7,7 @@ let xRandomVariance = 0;
 let yRandomVariance = 0;
 
 let drawCount = 100;
+let c: p5.Color;
 
 /**
  * setup
@@ -15,36 +16,47 @@ function setup() {
   console.log(">setup");
 
   createCanvas(windowWidth, windowHeight);
-  background("red");
+  background("white");
 
   nb_sections = 10;
   margin = 50;
   lineHeight = windowHeight * 0.45;
   section_lenght = (windowWidth - (2 * margin)) / nb_sections;
 
+  xRandomVariance = 150;
+  yRandomVariance = 150;
+  drawCount = 500;
 
-  xRandomVariance = 50;
-  yRandomVariance = 50;
-  drawCount = 100;
+  let c = color("red");
+  c.setAlpha(20);
+  stroke(c);
+  noFill();
+  strokeWeight(1);
 
+  // let c = color("white");
+  // c.setAlpha(128);
+  // stroke(c);
+  // noFill();
+  // strokeWeight(2);
+
+  // beginShape();
   // for (let i = 0; i < nb_sections; i++) {
-  //   let c = color("white");
-  //   c.setAlpha(128);
-  //   stroke(c);
-  //   noFill();
-  //   strokeWeight(2);
+
   //   let x1 = margin + (i * section_lenght);
   //   let x2 = margin + ((i + 1) * section_lenght);
 
-  //   let randomWeight = (i + 1) / nb_sections;
-  //   console.log(`randomWeight ${randomWeight}`);
+  //   let randomWeight = i / nb_sections;
   //   let ctrlP1x = x1 + (randomGaussian(0, xRandomVariance)) * randomWeight;
-  //   let ctrlP1y = height + (randomGaussian(0, xRandomVariance)) * randomWeight;
+  //   let ctrlP1y = lineHeight + (randomGaussian(0, xRandomVariance * randomWeight)) * randomWeight;
   //   let ctrlP2x = x2 + (randomGaussian(0, xRandomVariance)) * randomWeight;
-  //   let ctrlP2y = height + (randomGaussian(0, xRandomVariance)) * randomWeight;
+  //   let ctrlP2y = lineHeight + (randomGaussian(0, xRandomVariance)) * randomWeight;
 
-  //   bezier(x1, lineHeight, ctrlP1x, ctrlP1y, ctrlP2x, ctrlP2y, x2, lineHeight);
+  //   curveVertex(x1, ctrlP1y);
+
   // }
+  // endShape();
+
+
 
   console.log("<setup");
 }
@@ -54,23 +66,24 @@ function setup() {
  */
 function draw() {
 
+
+
+  beginShape();
   for (let i = 0; i < nb_sections; i++) {
-    let c = color("white");
-    c.setAlpha(50);
-    stroke(c);
-    noFill();
-    strokeWeight(2);
+
     let x1 = margin + (i * section_lenght);
     let x2 = margin + ((i + 1) * section_lenght);
 
-    let randomWeight = (i) / nb_sections;
+    let randomWeight = i / nb_sections;
     let ctrlP1x = x1 + (randomGaussian(0, xRandomVariance)) * randomWeight;
-    let ctrlP1y = lineHeight + (randomGaussian(0, xRandomVariance)) * randomWeight;
+    let ctrlP1y = lineHeight + (randomGaussian(0, xRandomVariance * randomWeight)) * randomWeight;
     let ctrlP2x = x2 + (randomGaussian(0, xRandomVariance)) * randomWeight;
     let ctrlP2y = lineHeight + (randomGaussian(0, xRandomVariance)) * randomWeight;
 
-    bezier(x1, lineHeight, ctrlP1x, ctrlP1y, ctrlP2x, ctrlP2y, x2, lineHeight);
+    curveVertex(ctrlP1x, ctrlP1y);
+
   }
+  endShape();
 
   drawCount--;
   if (drawCount == 0) noLoop();
